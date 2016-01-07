e5864196baad145640e7c6a76ff50834b4fb24b2
Isaac <yzaak.silva@gmail.com>
2016-01-07 17:02:22 -0300

    informaçoes
    pega as informações dos dados lidos

---
+++ CozinhaIndustrialWeb/src/br/edu/ifba/se/cozinhaindustrial/Informacao.java
@@ -0,0 +1,31 @@
+package br.edu.ifba.se.cozinhaindustrial;
+
+public class Informacao {
+	private int temperatura;
+	private int chamas;
+	private int gas;
+
+	public int getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(int temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public int getChamas() {
+		return chamas;
+	}
+
+	public void setChamas(int chamas) {
+		this.chamas = chamas;
+	}
+
+	public int getGas() {
+		return gas;
+	}
+
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+}
